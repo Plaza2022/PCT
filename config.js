941a5e2553680bf6d4e9ffc7f25396641ea51498
Plaza2022 <plaza.china.zou@gmail.com>
2026-07-18 16:52:16 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const APP_CONFIG = {
+
+    appName: "Plaza China Translator",
+
+    version: "1.0.0",
+
+    company: "PLAZA CHINA",
+
+    maxCharacters: 5000,
+
+    defaultTranslationType: "informal"
+
+};
